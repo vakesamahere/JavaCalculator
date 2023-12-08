c68561703f6d1ff5d613b315babf6b8bf57dad3d
vakesamahere <vakegamer@163.com>
2023-12-08 23:56:59 +0800

    finalMaybe

--- src/MyCalculator/Tools/ComponentEditor.java
+++ src/MyCalculator/Tools/ComponentEditor.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class ComponentEditor {
     public static void initializeComponentBody(JComponent self,JComponent father,double posX,double posY,double sizeX,double sizeY){//set the relative position and the relative size to the container
         
-        father.add(self);
+        if(self.getParent()==null||!self.getParent().equals(father))father.add(self);
         Rectangle rectangle = father.getBounds();
         double width = rectangle.width;
         double height = rectangle.height;
